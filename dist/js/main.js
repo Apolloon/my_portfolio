07f249cf15eadeb8b6cc5020bcfc08f8e39e573f
Apolloon <janielapollo12@gmail.com>
2019-08-01 18:48:52 +0800

    Added Project Details

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,27 +1,37 @@
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav');
-const navItems = document.querySelectorAll('.nav-item');
+$(document).ready(function () {
+    const menuBtn = document.querySelector('.menu-btn');
+    const menu = document.querySelector('.menu');
+    const menuNav = document.querySelector('.menu-nav');
+    const navItems = document.querySelectorAll('.nav-item');
 
-//State of the menu
-let showMenu = false;
+    //State of the menu
+    let showMenu = false;
 
-menuBtn.addEventListener('click', toggleMenu);
+    menuBtn.addEventListener('click', toggleMenu);
 
-function toggleMenu() {
-    if (!showMenu) {
-        menuBtn.classList.add('close');
-        menu.classList.add('show');
-        menuNav.classList.add('show');
-        navItems.forEach(item => item.classList.add('show'));
+    function toggleMenu() {
+        if (!showMenu) {
+            menuBtn.classList.add('close');
+            menu.classList.add('show');
+            menuNav.classList.add('show');
+            navItems.forEach(item => item.classList.add('show'));
 
-        showMenu = true;
-    } else {
-        menuBtn.classList.remove('close');
-        menu.classList.remove('show');
-        menuNav.classList.remove('show');
-        navItems.forEach(item => item.classList.remove('show'));
+            showMenu = true;
+        } else {
+            menuBtn.classList.remove('close');
+            menu.classList.remove('show');
+            menuNav.classList.remove('show');
+            navItems.forEach(item => item.classList.remove('show'));
 
-        showMenu = false;
+            showMenu = false;
+        }
     }
-}
+    // Work
+    //const items = document.querySelectorAll('.item');
+    $('div.descriptions').hide();
+
+    $('.item').hover(function () {
+        $(this).children('div.descriptions').slideToggle(500);
+    });
+
+});
